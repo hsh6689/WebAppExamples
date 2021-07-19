f07144d895ee7b52913a97cf4acf0f5d837b4537
IMEX Transport <admin@imextransport.com>
2021-07-19 11:59:09 -0700

    HW4 in progress4

--- Javascript Quiz/assets/js/script.js
+++ Javascript Quiz/assets/js/script.js
@@ -41,7 +41,6 @@ const myQuestions = [
   }
 ];
 
-var questionEl=document.getElementById("questions");
 var timerEl=document.getElementById("timer");
 
 //Div elements
@@ -56,66 +55,56 @@ var resultScore=document.querySelector(".result-score");
 
 
 //get values of answers
-var ans1=document.getElementById('ans1');
-var ans2=document.getElementById('ans2');
-var ans3=document.getElementById('ans3');
-var ans4=document.getElementById('ans4');
+
 
 function init(){
-		quizDiv.textContent="";
 		resultDiv.textContent	="";
 }
 
 
 
 function startGame(){
+		startTimer();
 		codingQuiz();
+}
+
+function startTimer(){
+	var solved = false; 
+	var timeLeft = 9;
+	var timeInterval = setInterval(function () {
+	  timerEl.textContent = timeLeft;
+	  timeLeft--;
+	  //if time is over, show result
+	  if(timeLeft===0){
+		  clearInterval(timeInterval);
+		  showResult();
+	  }
+  },1000);
 }
 
 //The codingQuiz function is called when the start button is clicked
 function codingQuiz(){
-	var solved = false; 
-	var timeLeft = 10;
-	var timeInterval = setInterval(function () {
-	  timerEl.textContent = timeLeft;
-	  timeLeft--;
-	//if time is over, show result
-	if(timeLeft===0){
-		clearInterval(timeInterval);
-		showResult();
-	}
-	//if not, show questions
-	else {
-	  for(var i=0; i<myQuestions.length; i++){
-	    questionEl.textContent=myQuestions[i].question;
-		  ans1.textContent=myQuestions[i].answers["a"];
-		  ans2.textContent=myQuestions[i].answers["b"];
-		  ans3.textContent=myQuestions[i].answers["c"];
-		  ans4.textContent=myQuestions[i].answers["d"];
-		
-	  }
-	}
+  for(var i=0; i<myQuestions.length; i++){
+  	var ans1=document.createElement("button");
+  	var ans2=document.createElement("button");
+		var ans3=document.createElement("button");
+		var ans4=document.createElement("button");
+		var question=document.createElement("h3");
 
-  },1000);
-}
+    question.textContent=myQuestions[i].question;
+	  ans1.textContent=myQuestions[i].answers["a"];
+	  ans2.textContent=myQuestions[i].answers["b"];
+	  ans3.textContent=myQuestions[i].answers["c"];
+	  ans4.textContent=myQuestions[i].answers["d"];
 
-function showQuestions(questions, quizContiner){
-	//we need a place to store teh output and teh answer choices
-	var output=[];
-	var answers; 
-	for(var i=0; i<myQuestions.length; i++){
-			answers=[];
+	  quizDiv.appendChild(question);
+		quizDiv.appendChild(ans1);
+		quizDiv.appendChild(ans2);
+		quizDiv.appendChild(ans3);
+		quizDiv.appendChild(ans4);
 
-
-	      questionEl.textContent=myQuestions[i].question;
-		  ans1.textContent=myQuestions[i].answers["a"];
-		  ans2.textContent=myQuestions[i].answers["b"];
-		  ans3.textContent=myQuestions[i].answers["c"];
-		  ans4.textContent=myQuestions[i].answers["d"];
-		
-	  }
-
-
+	  console.log("This is : " + ans1.textContent);
+  }
 }
 
 function showResult() {
@@ -124,20 +113,6 @@ function showResult() {
 	resultScore.textContent="result";
 }
 
-
-//Helper functions for 4 answer buttons 
-ans1.addEventListener("click", function(event) {
-	answerQuiz(0);
-});
-ans2.addEventListener("click", function(event) {
-	answerQuiz(1);
-});
-ans3.addEventListener("click", function(event) {
-	answerQuiz(2);
-});
-ans4.addEventListener("click", function(event) {
-	answerQuiz(3);
-});
 
 startButton.addEventListener("click", startGame);
 init();
